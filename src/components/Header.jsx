c5cc9162c1ec424c80aa20751a85e8a81649a92e
gautamkgit727 <gautamk727@gmail.com>
2026-03-17 10:54:45 +0530

    Mobile view header issue resolve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import './Header.css'
 import SearchTop from './SearchTop';
+import { useCommonData } from "../context/CommonContext";
 const Header = () => {
+  const { sidebarToggle } = useCommonData();
   // const recentSearches = [
   //   { img: "user1.jpg", name: "Danial Carabal" },
   //   { img: "user2.jpg", name: "Maria K" },
@@ -46,7 +48,7 @@ const Header = () => {
           </div>
         </div>
         <div className="right-compact">
-          <div className="sidemenu">
+          <div className="sidemenu" onClick={sidebarToggle}>
             <i>
               <svg id="side-menu2" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu">
                 <line x1="3" y1="12" x2="21" y2="12"></line>
